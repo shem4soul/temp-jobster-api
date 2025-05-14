5124a714d1f0c759613f1f30fce4064eace601ae
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-14 16:44:28 +0100

    Add user authentication details to request object and log user info in updateUser

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,7 +13,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    const testUser = payload.userId === '68220a57b2891479550c07b5'
+    req.user = { userId: payload.userId, testUser}
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
