10fa21a01113fbdd608f308b6bb972bbb2198fe4
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-14 12:37:24 +0100

    Add pagination to getAllJobs function

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -35,12 +35,18 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
-  const jobs = await result;
+  
+
+const page = Number(req.query.page) || 1
+const limit = Number(req.query.limit) || 10     
+const skip = (page - 1) * limit
+
+result = result.skip(skip).limit(limit)
+
+const jobs = await result;
 
   res.status(StatusCodes.OK).json({ jobs, total: jobs.length });
 };
-
-
 
 const getJob = async (req, res) => {
   const {
