7dc3ff05e20f0d6c8cd0e7a4369bc81418be69ac
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-14 13:57:12 +0100

    Fix getAllJobs response to include totalJobs and numOfPages

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -38,12 +38,20 @@ const getAllJobs = async (req, res) => {
   
 
 const page = Number(req.query.page) || 1
-const limit = Number(req.query.limit) || 10     
+const limit = Number(req.query.limit) || 10    
 const skip = (page - 1) * limit
 
 result = result.skip(skip).limit(limit)
 
 const jobs = await result;
+
+const totalJobs = await Job.countDocuments(queryObject);
+const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs,
+    numOfPages,
+  });
 
   res.status(StatusCodes.OK).json({ jobs, total: jobs.length });
 };
