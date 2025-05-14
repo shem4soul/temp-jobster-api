db76dc3863af1cb0a718c221921f9265d0834da0
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-14 17:53:18 +0100

    Add rate limiting to authentication routes and set trust proxy in app

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,21 @@ const express = require('express')
 const router = express.Router()
 const authenticateUser = require('../middleware/authentication')
 
+const rateLimit = require('express-rate-limit')
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+})
+
 
 const { register, login, updateUser } = require('../controllers/auth')
 const testUser = require('../middleware/testUser')
 
 
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', apiLimiter, register)
+router.post('/login', apiLimiter, login)
 router.patch('/updateUser', authenticateUser, testUser, updateUser)
 
 // router.get('/logout', authenticateUser, logout)
