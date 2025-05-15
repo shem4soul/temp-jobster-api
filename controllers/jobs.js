f2fed794ea7b5718df1acd2f74b2a738a98fedd6
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-15 10:01:58 +0100

    Add moment.js dependency and update showStats function to aggregate job statistics

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,8 @@
 const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
+const mongoose = require('mongoose')
+const moment = require('moment')
 
 
 const getAllJobs = async (req, res) => {
@@ -53,7 +55,6 @@ const numOfPages = Math.ceil(totalJobs / limit);
     numOfPages,
   });
 
-  res.status(StatusCodes.OK).json({ jobs, total: jobs.length });
 };
 
 const getJob = async (req, res) => {
@@ -116,7 +117,16 @@ const deleteJob = async (req, res) => {
 }
 
 const showStats = async (req, res) => {
-  res
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: '$status',
+        count: { $sum: 1 } }
+    },
+  ])  
+  console.log(stats);
+    res
   .status(StatusCodes.OK)
   .json({defaultStats: {}, monthlyApplications: []})
 }
